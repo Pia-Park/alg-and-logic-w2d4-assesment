a36918c66be57c21f601baac6f154371fa53da20
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-09-24 11:27:19 -0700

    first answer

--- insertionSort.js
+++ insertionSort.js
@@ -8,9 +8,20 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45];
 
 
 const insertionSort = function(arr) {
-
+    for(let i = 0; i < arr.length; i++){
+        let current = arr[i];
+        let left = i - 1;
+        
+        while(arr[left] > current){
+            arr[left + 1] = arr[left];
+            arr[left] = current;
+            current = arr[left];
+            left--;
+        }
+    }
+    return arr;
 };
 
 console.log(insertionSort(arr1));
 
-console.log(insertionSort(arr2));
+// console.log(insertionSort(arr2));
